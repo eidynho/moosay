24544880466a79d1e550a5505360733f4af6f28e
Eidynho <zviniciuseidy2@gmail.com>
2022-06-07 22:40:42 -0300

    adding project base

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Test } from './components/test'
+export function App() {
+
+  return (
+    <div className="mx-auto sm:bg-yellow-600 md:bg-green-600">
+      <h1>Hello world</h1>
+      <Test />
+    </div>
+  )
+}
---
+++ src/components/test.tsx
@@ -0,0 +1,7 @@
+
+export function Test() {
+
+  return (
+    <h1>teste component</h1>
+  )
+}
