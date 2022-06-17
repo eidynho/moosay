88fa2027e49488e3ed902a0e0b9196b2609cd45d
Eidynho <zviniciuseidy2@gmail.com>
2022-06-17 12:26:54 -0300

    refactoring + adding linux

--- src/components/Cow.tsx
+++ src/components/Cow.tsx
@@ -1,37 +1,58 @@
 import { useContext } from 'react'
+import { AnimalContext } from '../contexts/AnimalContext'
 import { MessageContext } from '../contexts/MessageContext'
 
 export function Cow() {
+  const { animal } = useContext(AnimalContext)
   const { message } = useContext(MessageContext)
   const emptyMessage = message.trim() === ''
+
+  const cow = (
+    <span>
+      &nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;^__^ <br />&nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;(oo)\________ <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(__)\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)\/\ <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;||----w&nbsp;| <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;||
+    </span>
+  )
+
+  const linux = (
+    <span>
+      &nbsp;&nbsp;&nbsp;\<br />&nbsp;&nbsp;&nbsp;&nbsp;\<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.--.<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|o_o |<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|:_/ |<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;\&nbsp;\<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;)<br />&nbsp;&nbsp;&nbsp;&nbsp;/'\_&nbsp;&nbsp;&nbsp;_/`\<br />&nbsp;&nbsp;&nbsp;&nbsp;\___)=(___/
+    </span>
+  )
 
   function maxRepeatMessageWall() {
     if (message.length <= 22) {
       return message.length + 1
     } else {
-      return 29
+      return 28
+    }
+  }
+
+  function animalInScreen() {
+    if (animal === 'cow') {
+      return cow
+    }
+
+    if (animal === 'linux') {
+      return linux
     }
   }
 
   return (
     <>
-    {
-      !emptyMessage ? (
-        <div className="py-4 px-8 max-w-xs text-justify">
-          <span className="font-mono leading-5 break-words">
-            {'_'.repeat(maxRepeatMessageWall())} <br />
-            {message}<br />
-            {'-'.repeat(maxRepeatMessageWall())} <br />
-            &nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;&nbsp;^__^ <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;\&nbsp;&nbsp;(oo)\________ <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(__)\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)\/\ <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;||----w&nbsp;| <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;||
-          </span>
-        </div>
-      ) : null
-    }
-      
+      {
+        !emptyMessage ? (
+          <div className="py-4 px-8 max-w-xs text-justify">
+            <span className="font-mono leading-5 break-words">
+              <>
+                {'_'.repeat(maxRepeatMessageWall())} <br />
+                {message}<br />
+                {'-'.repeat(maxRepeatMessageWall())} <br />
+                {animalInScreen()}
+              </>
+            </span>
+          </div>
+        ) : null
+      }
     </>
   )
 }
