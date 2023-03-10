39e46185e85446b8bcb20180d7ccc8e1884b27ce
eidy <zviniciuseidy2@gmail.com>
2023-03-09 22:41:26 -0300

    new: font-weight 500 and 600 (Manrope)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default function Document() {
                 />
 
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;700&family=Space+Mono&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700&family=Space+Mono&display=swap"
                     rel="stylesheet"
                 />
 
