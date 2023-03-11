3c91466fc9c53ebb0a68a071814c372b0291fff0
eidy <zviniciuseidy2@gmail.com>
2023-03-11 18:23:11 -0300

    new: vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,25 @@
-import "@/styles/tailwind.css";
 import { AppProps } from "next/app";
-import { globalStyles } from "../styles/global";
+import { Analytics } from "@vercel/analytics/react";
 
 import { AnimalContextProvider } from "@/contexts/AnimalContext";
 import { MessageContextProvider } from "@/contexts/MessageContext";
 import { NavBarComponent } from "@/components/NavBar";
 
+import "@/styles/tailwind.css";
+import { globalStyles } from "../styles/global";
+
 globalStyles();
 
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <AnimalContextProvider>
             <MessageContextProvider>
                 <div className="md:container mx-auto h-screen">
                     <NavBarComponent />
                     <Component {...pageProps} />
+                    <Analytics />
                 </div>
             </MessageContextProvider>
         </AnimalContextProvider>
     );
 }
-
-export default App;
