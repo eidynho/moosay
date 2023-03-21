ad71dded125cc4a8813ef8a8deb6e17f4cde4bdb
eidy <zviniciuseidy2@gmail.com>
2023-03-21 19:36:41 -0300

    new: Footer component

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,20 @@
+export function Footer() {
+    return (
+        <footer className="mx-4">
+            <hr className="h-1 my-4" />
+            <div className="flex flex-col md:flex-row items-end justify-between gap-3 py-2 mb-4 font-mono text-end text-sm">
+                <span>Copyright © 2023 Vinicius Eidy Okuda</span>
+                <span>
+                    Built by{" "}
+                    <a
+                        href="https://www.linkedin.com/in/eidy/"
+                        target="_blank"
+                        className="underline"
+                    >
+                        Vinicius Eidy Okuda
+                    </a>
+                </span>
+            </div>
+        </footer>
+    );
+}
